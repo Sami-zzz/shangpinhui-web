dd9fb0c012ea8851900a27fa492d9f801dc385c6
Sami-zzz <2787621724@qq.com>
2022-11-23 14:19:49 +0800

    使用mockjs模拟数据

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+
 import router from '@/router'
 import store from '@/store'
+
 import TypeNav from '@/components/TypeNav'
+
+import '@/mock/mockServe'
 
 Vue.component(TypeNav.name, TypeNav)
 
